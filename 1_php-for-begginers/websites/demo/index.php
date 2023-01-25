932d1705adc599c411d7935056573a5f47d263ad
Alexandre Mucarzel Fonseca <amfonseca98@gmail.com>
2023-01-25 14:39:46 -0300

    Finished Section 1

--- 1_php-for-begginers/websites/demo/index.php
+++ 1_php-for-begginers/websites/demo/index.php
@@ -1,54 +1,28 @@
-<!DOCTYPE html>
+<?php
 
-<html lang="en">
+$books = [
+    [
+        'name' => 'Do Androids Dream of Electric Sheep',
+        'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
+        'purchaseUrl' => 'http://example.com'
+    ]
+];
 
-<head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
-</head>
+$filteredBooks = array_filter($books, function ($book) {
+    return $book['author'] === "Andy Weir";
+});
 
-<body>
-    <h1>Recommended Books</h1>
-
-    <?php
-    $books = [
-        [
-            'name' => 'Do Androids Dream of Electric Sheep',
-            'author' => 'Philip K. Dick',
-            'releaseYear' => 1968,
-            'purchaseUrl' => 'http://example.com'
-        ],
-        [
-            'name' => 'Project Hail Mary',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2021,
-            'purchaseUrl' => 'http://example.com'
-        ],
-        [
-            'name' => 'The Martian',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2011,
-            'purchaseUrl' => 'http://example.com'
-        ]
-    ];
-
-    function filterByAuthor($books)
-    {
-        
-
-        return 'Gibberish';
-    }
-    ?>
-
-    <?php foreach ($books as $book) : ?>
-        <li>
-            <a href="<?= $book['purchaseUrl'] ?>">
-                <?= $book['name'] ?> (<?= $book['releaseYear'] ?>), by <?= $book['author'] ?>
-            </a>
-        </li>
-    <?php endforeach ?>
-
-    <?= filterByAuthor($books) ?>
-</body>
-
-</html>
+require "index.view.php";
