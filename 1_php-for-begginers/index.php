b9ba15e071ed60aec7a074c6930cb2485829d833
Alexandre Mucarzel Fonseca <amfonseca98@gmail.com>
2023-01-26 10:14:28 -0300

    Finished Section 2

--- 1_php-for-begginers/index.php
+++ 1_php-for-begginers/index.php
@@ -1,5 +1,15 @@
 <?php
 
 require 'functions.php';
+// require 'router.php';
+require 'Database.php';
 
-require 'router.php';
+$config = require('config.php');
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+$query = "SELECT * FROM posts WHERE id = :id";
+
+$posts = $db->query($query, [':id' => $id])->fetch();
+
+dd($posts);
